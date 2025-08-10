dd497b9e8ef8da6d08db22f1d6eeae73f124c056
ascent <ascent@ascents-MacBook-Air.local>
2025-08-10 19:31:09 +0600

    something is added

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,7 @@ export const metadata = {
   description: "",
  icons: {
     icon: '/favicon.ico',
+    shortcut: "/favicon.png",
   },
 };
 
@@ -23,7 +24,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Add the Google Fonts link here */}
-        <link rel="icon" href="https://www.bookforest.net/favicon.ico" />
+         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Marcellus&family=Jost:wght@400;700&family=Roboto:wght@400;700&family=Space+Mono&family=Montserrat&family=Courier+Prime&family=Poppins:wght@400;700&family=Raleway:wght@400;700&family=Nunito+Sans:wght@400;700&family=Playfair+Display:wght@400;700&family=Lora:wght@400;700&family=Abril+Fatface&family=Pacifico&display=swap"
